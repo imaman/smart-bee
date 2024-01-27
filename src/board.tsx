50b5e4eec5119dbffd0c87c582661ea3182cbbe0
Itay Maman <94941+imaman@users.noreply.github.com>
2024-01-27 13:01:55 +0200

    skip

--- src/board.tsx
+++ src/board.tsx
@@ -11,22 +11,22 @@ export function Board(props: {callback: (x: number, y: number) => 'none' | 'solv
   const content = (x: number, y: number) => {
     const s = props.callback(x, y)
     if (s === 'none') {
-      return <div className="cell"></div>
+      return <div key={x*y} className="cell"></div>
     }
 
     if (s === 'ask') {
-      return <div className="cell ask">{`${x} x ${y} = ?`}</div>
+      return <div key={x*y} className="cell ask">{`${x} x ${y} = ?`}</div>
     }
 
     if (s === 'solve') {
-      return <div className="cell">{`${x*y}`}</div>
+      return <div key={x*y} className="cell">{`${x*y}`}</div>
     }
 
     throw new Error(`illegal`)
   }
   return <div className="board">
     <div></div>
-    {arrOf(10, 1).map(at => <div className="cell edge">{at}</div>)}
+    {arrOf(10, 1).map(at => <div key={at} className="cell edge">{at}</div>)}
     {arrOf(10, 1).map(y => <>
         <div className="cell edge">{y}</div>
         {arrOf(10, 1).map(x => content(x,y))}
